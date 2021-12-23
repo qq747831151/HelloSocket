2d98051e0e4fc02a53eecfec38989b1c33b58585
qq747831151 <747831151@qq.com>
2021-12-23 15:36:49 +0800

    这次用了内存池 没有报错

--- HelloSocket/EasyTcpServer/Alloc.cpp
+++ HelloSocket/EasyTcpServer/Alloc.cpp
@@ -1,23 +1,23 @@
-//#pragma  once
-//#include "Alloc.h"
-//#include "MemoryMgr.hpp"
-//void* operator new(size_t size)
-//{
-//
-//	return MemoryMgr::Instance().allocMem(size);
-//}
-//void operator delete(void* p) {
-//	MemoryMgr::Instance().freeMem(p);
-//}
-//void* operator new[](size_t size) {
-//	return MemoryMgr::Instance().allocMem(size);
-//}
-//void operator delete[](void* p) {
-//	MemoryMgr::Instance().freeMem(p);
-//}
-//void* mem_alloc(size_t size) {
-//	return MemoryMgr::Instance().allocMem(size);
-//}
-//void mem_free(void* p) {
-//	MemoryMgr::Instance().freeMem(p);
-//}
+
+#include "Alloc.h"
+#include "MemoryMgr.hpp"
+void* operator new(size_t size)
+{
+
+	return MemoryMgr::Instance().allocMem(size);
+}
+void operator delete(void* p) {
+	MemoryMgr::Instance().freeMem(p);
+}
+void* operator new[](size_t size) {
+	return MemoryMgr::Instance().allocMem(size);
+}
+void operator delete[](void* p) {
+	MemoryMgr::Instance().freeMem(p);
+}
+void* mem_alloc(size_t size) {
+	return MemoryMgr::Instance().allocMem(size);
+}
+void mem_free(void* p) {
+	MemoryMgr::Instance().freeMem(p);
+}
